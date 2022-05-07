04938d6fc8bbc19c47691aad1eb8b6dd4f44138b
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-05-07 23:53:18 +0200

    feat: Implemented profile image change

--- XWSBack/BaseApi/Controllers/UpdateUserController.cs
+++ XWSBack/BaseApi/Controllers/UpdateUserController.cs
@@ -1,16 +1,14 @@
 ﻿using AutoMapper;
 using BaseApi.CustomAttributes;
-using BaseApi.Dto.Milestone;
 using BaseApi.Dto.Users;
 using BaseApi.Model.Mongo;
-using BaseApi.Services.Exceptions;
 using BaseApi.Services.PictureServices;
 using BaseApi.Services.UserServices;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BaseApi.Controllers
@@ -25,13 +23,15 @@ namespace BaseApi.Controllers
         private readonly IMapper _mapper;
         private readonly EditUserService _editUserService;
         private readonly SignInManager<User> _signInManager;
+        private readonly PictureService _pictureService;
 
-        public UpdateUserController(UserManager<User> userManager, IMapper mapper, EditUserService editUserService, SignInManager<User> signInManager)
+        public UpdateUserController(UserManager<User> userManager, IMapper mapper, EditUserService editUserService, SignInManager<User> signInManager, PictureService pictureService)
         {
             _userManager = userManager;
             _mapper = mapper;
             _editUserService = editUserService;
             _signInManager = signInManager;
+            _pictureService = pictureService;
         }
 
         [HttpPut]
@@ -58,6 +58,22 @@ namespace BaseApi.Controllers
                 return BadRequest(result.Errors);
 
             return Ok("Password succesfully changed!");
+        }
+
+        [HttpPut("profileImg")]
+        public async Task<IActionResult> ChangePhoto(IFormFile picture)
+        {
+            var userId = Guid.Parse(_userManager.GetUserId(User));
+            _pictureService.DeletePostPicture(userId);
+            if (picture != null && picture.Length != 0)
+            {
+                using var ms = new MemoryStream();
+                await picture.CopyToAsync(ms);
+
+                _pictureService.SaveUserPicture(userId, ms.ToArray());
+            }
+
+            return Ok("Image changed successfully");
         }
 
     }
