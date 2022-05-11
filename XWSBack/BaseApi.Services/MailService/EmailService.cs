b3ada91f71e7e7f8274a02a9d1477864c0edb7b0
NikolaRA5 <milosavljevic.ra5.2018@uns.ac.rs>
2022-05-11 19:16:25 +0200

    implemented account recovery

--- XWSBack/BaseApi.Services/MailService/EmailService.cs
+++ XWSBack/BaseApi.Services/MailService/EmailService.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
-namespace BaseApi.Services.EmailService
+namespace BaseApi.Services.MailService
 {
     public class EmailService
     {
@@ -16,9 +16,11 @@ namespace BaseApi.Services.EmailService
         private const string VerificationTemplate = "d-ce89fa0a658b45b9bfd46ed0712e1cc8";
         private const string VerificationLinkTemplate = "https://localhost:44322/api/Register/Confirm?userId={0}&token={1}";
 
-        private const string SuccessfulVerificationMail = "Verification email sent to mail address : {0}";
-        private const string FailedVerificationMail = "Couldn't send verification email to mail address : {0}";
-        
+        private const string RecoveryTemplate = "d-da61904271ad475ead15b4663004dabc";
+
+        private const string SuccessfulMail = "Mail sent to mail address : {0}";
+        private const string FailedMail = "Couldn't send email to mail address : {0}";
+
         public EmailService(SendGridContract sendGridContract, ILogger<EmailService> logger)
         {
             _sendGridContract = sendGridContract;
@@ -27,7 +29,6 @@ namespace BaseApi.Services.EmailService
         
         public async Task VerifyEmail(string email, Guid userId, string token)
         {
-            _client = new SendGridClient(_sendGridContract.Secret);
             var message = new SendGridMessage()
             {
                 From = new EmailAddress(_sendGridContract.From),
@@ -39,14 +40,38 @@ namespace BaseApi.Services.EmailService
                 buttonUrl = string.Format(VerificationLinkTemplate, userId, token)
             });
 
+            await SendTemplatedMail(message, email);
+        }
+
+        public async Task RecoverAccount(string email, string token)
+        {
+            var message = new SendGridMessage()
+            {
+                From = new EmailAddress(_sendGridContract.From),
+                TemplateId = RecoveryTemplate
+            };
+            message.AddTo(email);
+            message.SetTemplateData(new
+            {
+                token
+            });
+
+            await SendTemplatedMail(message, email);
+        }
+
+        private async Task SendTemplatedMail(SendGridMessage message, string email)
+        {
+            _client = new SendGridClient(_sendGridContract.Secret);
+
             var response = await _client.SendEmailAsync(message);
+            
             if (response.IsSuccessStatusCode)
             {
-                _logger.LogInformation(string.Format(SuccessfulVerificationMail, email));
+                _logger.LogInformation(string.Format(SuccessfulMail, email));
             }
             else
             {
-                var formatted = string.Format(FailedVerificationMail, email);
+                var formatted = string.Format(FailedMail, email);
                 _logger.LogInformation(formatted);
                 _logger.LogInformation(await response.Body.ReadAsStringAsync());
                 throw new Exception(formatted);
---
+++ XWSBack/BaseApi/Controllers/RecoveryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using BaseApi.Dto.Users;
+using BaseApi.Services.UserServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecoveryController : ControllerBase
+    {
+        private readonly RecoveryService _recoveryService;
+
+        public RecoveryController(RecoveryService recoveryService)
+        {
+            _recoveryService = recoveryService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(string email)
+        {
+            try
+            {
+                await _recoveryService.Recover(email);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpPost("ResetPassword")]
+        public async Task<IActionResult> ResetPassword(RecoverPasswordDto passwordDto)
+        {
+            try
+            {
+                await _recoveryService.ResetPassword(passwordDto.Email, passwordDto.Token, passwordDto.NewPassword);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
