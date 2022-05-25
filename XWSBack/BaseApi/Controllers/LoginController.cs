c04e3b684191c45b0d01faf5bcbb054b751f6e8d
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-05-25 02:11:07 +0200

    fix: Changed passwordless redirection

--- XWSBack/BaseApi/Controllers/LoginController.cs
+++ XWSBack/BaseApi/Controllers/LoginController.cs
@@ -21,7 +21,7 @@ namespace BaseApi.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
             _loginService = loginService;
-            _path = Environment.GetEnvironmentVariable("XWS_FRONT_PATH") ?? "http://localhost:3000";
+            _path = Environment.GetEnvironmentVariable("XWS_FRONT_PATH") ?? "http://localhost:3000/loggedIn";
         }
 
         [HttpPost("api/login")]
