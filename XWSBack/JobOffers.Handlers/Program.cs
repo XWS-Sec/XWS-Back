b7bb445d41b5140bbffe0498341e015a5fb6bcea
Nikola Milosavljevic <nikola.milosavljevic@schneider-electric-dms.com>
2022-05-31 15:36:43 +0200

    created jobOffer microservice

---
+++ XWSBack/JobOffers.Handlers/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using JobOffers.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using NServiceBus;
+using Serilog;
+using Serilog.Events;
+using Shared;
+
+namespace JobOffers.Handlers
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.WithNsbExceptionDetails().FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/Chats.Handlers.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+        
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseConsoleLifetime()
+                .UseSerilog()
+                .ConfigureServices(services =>
+                {
+                    var serviceInstances = typeof(Program).Assembly.GetTypes()
+                        .Where(x => x.Namespace.EndsWith("Services") && x.Name.EndsWith("Service"));
+
+                    foreach (var serviceInstance in serviceInstances)
+                    {
+                        services.AddSingleton(serviceInstance);
+                    }
+                    
+                    services.AddSingleton<IMongoClient, MongoClient>(s => SetupMongoDb.CreateClient<Company>("JobOffersHandlers", "JobOffers"));
+                    BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(BsonType.String));
+                }).UseNServiceBus(ctx =>
+                {
+                    var endpointConfig = new EndpointConfiguration(EndpointInstances.ChatHandlers);
+                    var routing = endpointConfig.Configure(EndpointInstances.ChatHandlers);
+
+                    return endpointConfig;
+                });
+        }
+    }
+}
---
+++ XWSBack/JobOffers.Model/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobOffers.Model
+{
+    public class Company
+    {
+        public Guid InternalId { get; set; }
+        public Guid ExternalCorrelation { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string ApiKeyHash { get; set; }
+
+        public IList<JobOffer> JobOffers { get; set; }
+    }
+}
