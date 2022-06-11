ad5659bf7a707ca0794255e97c23076317724544
NikolaRA5 <milosavljevic.ra5.2018@uns.ac.rs>
2022-06-11 14:54:22 +0200

    adding serializiable attributes to custom exceptions

--- XWSBack/BaseApi.Services/Exceptions/EmailingException.cs
+++ XWSBack/BaseApi.Services/Exceptions/EmailingException.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace BaseApi.Services.Exceptions
 {
+    [Serializable]
     public class EmailingException : Exception
     {
         public EmailingException(string message) : base(message)
+        {
+        }
+
+        protected EmailingException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
--- XWSBack/BaseApi.Services/Exceptions/FacebookException.cs
+++ XWSBack/BaseApi.Services/Exceptions/FacebookException.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace BaseApi.Services.Exceptions
 {
+    [Serializable]
     public class FacebookException : Exception
     {
         public FacebookException(string message) : base(message)
+        {
+        }
+
+        protected FacebookException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
