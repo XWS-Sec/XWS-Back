a0c852d39cfc9c4f44dcbb030f634b081e2a71ae
NikolaRA5 <milosavljevic.ra5.2018@uns.ac.rs>
2022-06-14 00:24:14 +0200

    added follow recommendation system

---
+++ XWSBack/Users.Graph.Messages/Follow/RecommendNewLinksResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Shared.Custom;
+
+namespace Users.Graph.Messages.Follow
+{
+    public class RecommendNewLinksResponse : ICustomMessage
+    {
+        public Guid CorrelationId { get; set; }
+        public IEnumerable<Guid> Recommended { get; set; }
+        public bool IsSuccessful { get; set; }
+        public string MessageToLog { get; set; }
+    }
+}
